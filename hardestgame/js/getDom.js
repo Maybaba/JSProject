7901d50082262295ed11688a4b4dd23b1eaac29d
Maybaba <sinyunjong@gmail.com>
2024-04-08 14:53:54 +0900

    wrong access

---
+++ hardestgame/js/getDom.js
@@ -0,0 +1,23 @@
+
+// 캐릭터
+const $char = document.querySelector('.character');
+
+// 파란 공
+const $avoid = [...document.querySelectorAll('.avoidCircle')];
+
+// 빨간 박스
+const $redBox = document.querySelector(".box");
+
+// 클리어 시 삭제에 필요한 변수
+const $game = document.querySelector('.game')
+
+// 클리어존
+const $clearZone = document.querySelector(".clearZone");
+
+// 클리어존의 x(가로)좌표 값을 변수에 담는 과정
+const $clearZoneCoor = $clearZone.getBoundingClientRect();
+console.log(Math.floor($clearZoneCoor.x));
+const $clear = $clearZoneCoor.x;
+
+export { $char, $avoid, $redBox, $game, $clearZone, $clearZoneCoor, $clear };
+
