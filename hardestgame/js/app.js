8783d428089841d4fa2fd218e0382e78b1f884b2
kimbyeongmo <kbmomo9999@naver.com>
2024-04-09 20:48:17 +0900

    "영역 안에서만 이동 구현 성공"

--- hardestgame/js/app.js
+++ hardestgame/js/app.js
@@ -1,7 +1,9 @@
 import { clear } from "./makeFunction.js";
 import { detectCollision } from "./collideByYJ.js";
 import { $redBox, $eatCircle1, $clear } from "./getDom.js";
+import { hiddenClear, $hiddenClear } from "./hiddenClear.js";
 // import { intervalId } from './crush.js'
+$hiddenClear.addEventListener('click', hiddenClear);
 
 // document.addEventListener("keydown", function (event) {
 //   const $box = document.querySelector(".box");
@@ -10,54 +12,93 @@ import { $redBox, $eatCircle1, $clear } from "./getDom.js";
 //   const $boxTop = parseInt($boxStyle.top);
 //   const $boxWidth = parseInt($boxStyle.width);
 //   const $boxHeight = parseInt($boxStyle.height);
-//   const step = 15;
+//   const step = 10;
 
-//     switch (event.key) {
-//       case "ArrowLeft":
-//         $box.style.left = Math.max($boxLeft - step, 0) + "px";  
-//         break;
-//       case "ArrowUp":
+//   switch (event.key) {
+//     case "ArrowLeft":
+//       if (!checkCollision("left")) {
+//         $box.style.left = Math.max($boxLeft - step, 0) + "px";
+//       }
+//       break;
+//     case "ArrowUp":
+//       if (!checkCollision("up")) {
 //         $box.style.top = Math.max($boxTop - step, 0) + "px";
-//         break;
-//       case "ArrowRight":
+//       }
+//       break;
+//     case "ArrowRight":
+//       if (!checkCollision("right")) {
 //         $box.style.left =
 //           Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-//         break;
-//       case "ArrowDown":
+//       }
+//       break;
+//     case "ArrowDown":
+//       if (!checkCollision("down")) {
 //         $box.style.top =
 //           Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
-//         break;
-//       case "ArrowLeft":
-//         if (event.key === "ArrowLeft" && event.key === "ArrowUp") {
-//           $box.style.left = Math.max($boxLeft - step, 0) + "px";
-//           $box.style.top = Math.max($boxTop - step, 0) + "px";
+//       }
+//       break;
+//   }
+// });
+
+// function checkCollision(direction) {
+//   const $box = document.querySelector(".box");
+//   const $boxRect = $box.getBoundingClientRect();
+//   const $obstacles = document.querySelectorAll(".leftborder, .rightborder, .topborder, .bottomborder");
+
+//   let collision = false;
+
+//   $obstacles.forEach(function ($obstacle) {
+//     const obstacleRect = $obstacle.getBoundingClientRect();
+
+//     switch (direction) {
+//       case "left":
+//         if (
+//           $boxRect.left - 10 < obstacleRect.right &&
+//           $boxRect.right > obstacleRect.right &&
+//           $boxRect.top < obstacleRect.bottom &&
+//           $boxRect.bottom > obstacleRect.top
+//         ) {
+//           collision = true;
 //         }
 //         break;
-//       case "ArrowLeft":
-//         if (event.key === "ArrowLeft" && event.key === "ArrowDown") {
-//           $box.style.left = Math.max($boxLeft - step, 0) + "px";
-//           $box.style.top =
-//             Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
+//       case "up":
+//         if (
+      
+//           $boxRect.top - 10 < obstacleRect.bottom &&
+//           $boxRect.bottom > obstacleRect.bottom &&
+//           $boxRect.left < obstacleRect.right &&
+//           $boxRect.right > obstacleRect.left
+//         ) {
+//           collision = true;
 //         }
 //         break;
-//       case "ArrowRight":
-//         if (event.key === "ArrowRight" && event.key === "ArrowUp") {
-//           $box.style.left =
-//             Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-//           $box.style.top = Math.max($boxTop - step, 0) + "px";
+//       case "right":
+//         if (
+//           $boxRect.right + 10 > obstacleRect.left &&
+//           $boxRect.left < obstacleRect.left &&
+//           $boxRect.top < obstacleRect.bottom &&
+//           $boxRect.bottom > obstacleRect.top
+//         ) {
+//           collision = true;
 //         }
 //         break;
-//       case "ArrowRight":
-//         if (event.key === "ArrowRight" && event.key === "ArrowDown") {
-//           $box.style.left =
-//             Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-//           $box.style.top =
-//             Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
+//       case "down":
+//         if (
+//           $boxRect.bottom + 10 > obstacleRect.top &&
+//           $boxRect.top < obstacleRect.top &&
+//           $boxRect.left < obstacleRect.right &&
+//           $boxRect.right > obstacleRect.left
+//         ) {
+//           collision = true;
 //         }
 //         break;
 //     }
+//   });
 
-// });
+//   return collision;
+// }
+
+
 
 function isColliding(rect1, rect2) {
   return !(
--- hardestgame/js/moveInside.js
+++ hardestgame/js/moveInside.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("keydown", function (event) {
   const $box = document.querySelector(".box");
   const $boxStyle = getComputedStyle($box);
@@ -5,66 +6,88 @@ document.addEventListener("keydown", function (event) {
   const $boxTop = parseInt($boxStyle.top);
   const $boxWidth = parseInt($boxStyle.width);
   const $boxHeight = parseInt($boxStyle.height);
-  const step = 5;
-  const $left = document.querySelectorAll(".left");
+  const step = 10;
 
-
-  // console.log($left);
-  const leftArr=[];
-  for(const lf of $left){
-    leftArr.push(lf);
-  }
-  console.log()
-  setInterval(function () {
-    const left = $left.getBoundingClientRect().right;
-    const boxLeft = $box.getBoundingClientRect().left;
-
-    if (left >= boxLeft) {
-      console.log("왼쪽닿음");
-      switch (event.key) {
-        // case "ArrowLeft":
-        //   $box.style.left = Math.max($boxLeft - step, 0) + "px";
-        //   break;
-        case "ArrowUp":
-          $box.style.top = Math.max($boxTop - step, 0) + "px";
-          break;
-        case "ArrowRight":
-          $box.style.left =
-            Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-          break;
-        case "ArrowDown":
-          $box.style.top =
-            Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
-          break;
-        // case "ArrowLeft":
-        //   if (event.key === "ArrowLeft" && event.key === "ArrowUp") {
-        //     $box.style.left = Math.max($boxLeft - step, 0) + "px";
-        //     $box.style.top = Math.max($boxTop - step, 0) + "px";
-        //   }
-        //   break;
-        // case "ArrowLeft":
-        //   if (event.key === "ArrowLeft" && event.key === "ArrowDown") {
-        //     $box.style.left = Math.max($boxLeft - step, 0) + "px";
-        //     $box.style.top =
-        //       Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
-        //   }
-        //   break;
-        case "ArrowRight":
-          if (event.key === "ArrowRight" && event.key === "ArrowUp") {
-            $box.style.left =
-              Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-            $box.style.top = Math.max($boxTop - step, 0) + "px";
-          }
-          break;
-        case "ArrowRight":
-          if (event.key === "ArrowRight" && event.key === "ArrowDown") {
-            $box.style.left =
-              Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
-            $box.style.top =
-              Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
-          }
-          break;
+  switch (event.key) {
+    case "ArrowLeft":
+      if (!checkCollision("left")) {
+        $box.style.left = Math.max($boxLeft - step, 0) + "px";
       }
+      break;
+    case "ArrowUp":
+      if (!checkCollision("up")) {
+        $box.style.top = Math.max($boxTop - step, 0) + "px";
+      }
+      break;
+    case "ArrowRight":
+      if (!checkCollision("right")) {
+        $box.style.left =
+          Math.min(window.innerWidth - $boxWidth, $boxLeft + step) + "px";
+      }
+      break;
+    case "ArrowDown":
+      if (!checkCollision("down")) {
+        $box.style.top =
+          Math.min(window.innerHeight - $boxHeight, $boxTop + step) + "px";
+      }
+      break;
+  }
+});
+
+function checkCollision(direction) {
+  const $box = document.querySelector(".box");
+  const $boxRect = $box.getBoundingClientRect();
+  const $obstacles = document.querySelectorAll(".leftborder, .rightborder, .topborder, .bottomborder");
+
+  let collision = false;
+
+  $obstacles.forEach(function ($obstacle) {
+    const obstacleRect = $obstacle.getBoundingClientRect();
+
+    switch (direction) {
+      case "left":
+        if (
+          $boxRect.left - 10 < obstacleRect.right &&
+          $boxRect.right > obstacleRect.right &&
+          $boxRect.top < obstacleRect.bottom &&
+          $boxRect.bottom > obstacleRect.top
+        ) {
+          collision = true;
+        }
+        break;
+      case "up":
+        if (
+      
+          $boxRect.top - 10 < obstacleRect.bottom &&
+          $boxRect.bottom > obstacleRect.bottom &&
+          $boxRect.left < obstacleRect.right &&
+          $boxRect.right > obstacleRect.left
+        ) {
+          collision = true;
+        }
+        break;
+      case "right":
+        if (
+          $boxRect.right + 10 > obstacleRect.left &&
+          $boxRect.left < obstacleRect.left &&
+          $boxRect.top < obstacleRect.bottom &&
+          $boxRect.bottom > obstacleRect.top
+        ) {
+          collision = true;
+        }
+        break;
+      case "down":
+        if (
+          $boxRect.bottom + 10 > obstacleRect.top &&
+          $boxRect.top < obstacleRect.top &&
+          $boxRect.left < obstacleRect.right &&
+          $boxRect.right > obstacleRect.left
+        ) {
+          collision = true;
+        }
+        break;
     }
   });
-});
+
+  return collision;
+}
