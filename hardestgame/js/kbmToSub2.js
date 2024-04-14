30622cb75f30ae995443e93d4f103f7302286267
Maybaba <sinyunjong@gmail.com>
2024-04-14 10:14:54 +0900

    0414파일정리

--- hardestgame/js/kbmToSub2.js
+++ hardestgame/js/kbmToSub2.js
@@ -2,7 +2,7 @@
 import { $hiddenClear } from "./getDom.js";
 
 function nextLevel() {
-  window.location.href = "http://127.0.0.1:5500/hardestgame/html/kbm.html";
+  window.location.href = "http://127.0.0.1:5500/html/subPage2.html";
 }
 
 
